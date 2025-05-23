c9fc584afe424a47c74fb8dc79d6d3152bbfc01f
Ignacio Calderon <kronenthaler@gmail.com>
2025-05-23 17:10:46 +0200

    feat: add configuration class for scheduler

---
+++ src/test/kotlin/com/github/kronenthaler/ghasimulator/ConfigurationTest.kt
@@ -0,0 +1,23 @@
+package com.github.kronenthaler.ghasimulator
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ConfigurationTest {
+
+    @Test
+    fun `test getRunnerCount for label`() {
+        val configs = Configuration(20, listOf(Configuration.RunnerSpec("label1", 3)));
+        assertEquals(3, configs.getRunnerCount("label1"));
+        assertEquals(0, configs.getRunnerCount("label2"));
+    }
+
+    @Test
+    fun `test getRunnerLabels`() {
+        val configs = Configuration(20, listOf(
+                Configuration.RunnerSpec("label1", 3),
+                Configuration.RunnerSpec("label2", 5)
+        ));
+        assertEquals(listOf("label1", "label2"), configs.getRunnerLabels());
+    }
+}
