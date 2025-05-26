000edcf599014d4adecd186c3e372a5ed0e69dbb
kronenthaler <kronenthaler@gmail.com>
2025-05-26 21:56:38 +0200

    feat: add stats summary class

---
+++ src/test/kotlin/com/github/kronenthaler/ghasimulator/stats/StatsSummaryTest.kt
@@ -0,0 +1,26 @@
+package com.github.kronenthaler.ghasimulator.stats
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+class StatsSummaryTest {
+    @Test
+    fun `test stats summary`() {
+        val stats = listOf(
+            PipelineStats(0, 100, 10, 5),
+            PipelineStats(0, 200, 10, 5),
+            PipelineStats(0, 1000, 100, 5)
+        )
+
+        val summary = StatsSummary(stats, 1)
+
+        assertEquals("""
+            |Pipeline summary stats:
+            |                    	avg    	stdev  	pc50   	pc75   	min    	max    
+            |Run Time            	433.33 	402.77 	200    	1000   	100    	1000   
+            |Queue Time          	40.00  	42.43  	10     	100    	10     	100    
+            |
+        """.trimMargin(), summary.formattedSummary())
+    }
+}
