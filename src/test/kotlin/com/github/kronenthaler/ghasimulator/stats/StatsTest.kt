fd0bba991233c0a88b388934d3f76a5a85c870af
kronenthaler <kronenthaler@gmail.com>
2025-05-27 22:38:38 +0200

    fix: queue time scaling issue

--- src/test/kotlin/com/github/kronenthaler/ghasimulator/stats/StatsTest.kt
+++ src/test/kotlin/com/github/kronenthaler/ghasimulator/stats/StatsTest.kt
@@ -1,13 +1,12 @@
 package com.github.kronenthaler.ghasimulator.stats
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import kotlin.test.*
 
 class StatsTest {
     @Test
     fun `test output formatting pipeline stats`() {
         val stats = PipelineStats(123, 1000, 2000, 3000)
-        val expectedOutput = "${(1000-123)/20}\t123\t1000\t2000\t3000"
-        Assertions.assertEquals(expectedOutput, stats.toString(20))
+        val expectedOutput = "${(1000-123)/20}\t123\t1000\t100\t3000"
+        assertEquals(expectedOutput, stats.toString(20))
     }
 }
