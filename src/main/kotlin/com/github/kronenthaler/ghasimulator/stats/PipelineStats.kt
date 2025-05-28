5831e020cf74772a29302902eda3efb87f0dcba6
Ignacio Calderon <kronenthaler@users.noreply.github.com>
2025-05-28 09:52:22 +0200

    fix: queue time scaling issue (#16)

--- src/main/kotlin/com/github/kronenthaler/ghasimulator/stats/PipelineStats.kt
+++ src/main/kotlin/com/github/kronenthaler/ghasimulator/stats/PipelineStats.kt
@@ -2,6 +2,6 @@ package com.github.kronenthaler.ghasimulator.stats
 
 data class PipelineStats(val startTime: Long, val endTime: Long, val totalQueueTime: Long, val jobCount: Int) {
     fun toString(timescale: Int): String {
-        return "${(endTime - startTime) / timescale}\t$startTime\t$endTime\t$totalQueueTime\t$jobCount"
+        return "${(endTime - startTime) / timescale}\t$startTime\t$endTime\t${totalQueueTime / timescale}\t$jobCount"
     }
 }
